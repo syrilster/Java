9da2732c86d5ff88843a20fb29f905cc51508b0c
syrilster <syrilster@gmail.com>
2016-04-24 21:27:21 +0530

    Update and rename SinglyLinkedList.java to StringToNumber.java

---
+++ StringToNumber.java
@@ -0,0 +1,38 @@
+/**
+ * Created by Syril on 24-04-2016.
+ */
+public class StringToNumber {
+
+    public static void main(String[] args) {
+        System.out.print(getNumberFromString("-123"));
+    }
+
+    /**
+     * This works primarily because the characters 0-9 have consecutive ascii values, so subtracting '0' from
+     * any of them gives you the offset from the character '0', which is of course the numeric equivalent of the character.
+     * The multiplication by 10 and addition is just building up the number as you go.
+     **/
+
+    static int getNumberFromString(String s) {
+
+        if (s == null) {
+            throw new NumberFormatException("null");
+        }
+        int result = 0;
+        int j = 0;
+        int length = s.length();
+        boolean negative = false;
+        char firstChar = s.charAt(0);
+        if (firstChar == '-') {
+            negative = true;
+            j = 1;
+        }
+
+        for (int i = j; i < length; i++) {
+            char num = s.charAt(i);
+            result = result * 10 + num - '0';
+        }
+
+        return negative ? -result : result;
+    }
+}
