dc724cbdfeee950d031d0ac196c83337a0c34af2
syrilster <syrilster@gmail.com>
2016-05-15 19:54:45 +0530

    Create PermutationElem.java

---
+++ PermutationElem.java
@@ -0,0 +1,34 @@
+/**
+ * Created by Syril on 08-04-2016.
+ */
+public class PermutationElem {
+
+    public static void main(String[] args) {
+        permutation(new int[]{1, 2, 3}, 0);
+    }
+
+    static void permutation(int a[], int k) {
+        if (k == a.length) {
+            String number = "";
+            for (int num : a) {
+                number += num;
+            }
+
+            System.out.println(number);
+
+        } else {
+            for (int i = k; i < a.length; i++) {
+                int temp = a[k];
+                a[k] = a[i];
+                a[i] = temp;
+
+                permutation(a, k + 1);
+
+                temp = a[k];
+                a[k] = a[i];
+                a[i] = temp;
+
+            }
+        }
+    }
+}
