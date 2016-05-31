f8ce1786c0fb0062b3e51c29986495ec17661717
syrilster <syrilster@gmail.com>
2016-05-31 17:59:40 +0530

    Create ArrayDiagonal.java

---
+++ ArrayDiagonal.java
@@ -0,0 +1,34 @@
+/**
+ * Created by syrils on 5/26/16.
+ */
+public class ArrayDiagonal {
+    public static void main(String[] args) {
+        int array[][] = new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        printMatrixDiagonally(array);
+    }
+
+    public static void printMatrixDiagonally(int[][] matrix) {
+        int rowCount = matrix.length;
+        int colCount = matrix[0].length;
+        int row, col;
+
+        for (int k = 0; k < rowCount; k++) {
+            for (row = k, col = 0; row >= 0 && col < colCount; row--, col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
+
+        for (int k = 1; k < colCount; k++) {
+            for (row = rowCount - 1, col = k; row >= 0 && col < colCount; row--, col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
