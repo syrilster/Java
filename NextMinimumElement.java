2a9a4e8ac5faec05029dfbf5dc027a0a5e4513ee
syrilster <syrilster@gmail.com>
2017-02-03 09:33:42 +0530

    Update NextMinimumElement.java

--- NextMinimumElement.java
+++ NextMinimumElement.java
@@ -5,25 +5,22 @@ import java.util.Stack;
  */
 public class NextMinimumElement {
     public static void main(String[] args) {
-        int a[] = {4,2,1,5,3};
+        int a[] = {10, 7, 9, 8, 3, 5};
         getNGE(a);
     }
+
     public static void getNGE(int[] a) {
         Stack<Integer> s = new Stack<>();
         s.push(a[0]);
         for (int i = 1; i < a.length; i++) {
-            if (s.peek() != null) {
-                while (true) {
-                    if (s.isEmpty() || s.peek() < a[i]) {
-                        break;
-                    }
-                    if (!s.isEmpty()) System.out.println(s.pop() + ":" + a[i]);
-                }
+            while (!s.isEmpty() && s.peek() > a[i]) {
+                System.out.println("Next minimum element for "
+                        + s.pop() + "\t = " + a[i]);
             }
             s.push(a[i]);
         }
         while (!s.isEmpty() && s.peek() != null) {
-            System.out.println(s.pop() + ":" + -1);
+            System.out.println("Next minimum element for " + s.pop() + "\t = " + -1);
         }
     }
 }
