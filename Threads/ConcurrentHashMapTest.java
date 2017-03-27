ebb8c6f7463899e2d16c79375347a718e1893481
syrilster <syrilster@gmail.com>
2017-03-27 21:09:37 +0530

    Create ConcurrentHashMapTest.java

---
+++ Threads/ConcurrentHashMapTest.java
@@ -0,0 +1,31 @@
+public class ConcurrentHashMapTest {
+    private static Map<Integer, Integer> map = new ConcurrentHashMap<>();
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 3; i++) {
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 50; i++) {
+                        testMethod(i);
+                    }
+                    System.out.println("Map size " + map.size());
+                }
+            });
+        }
+
+        //executorService.shutdown();
+    }
+
+    public static void testMethod(Integer integer) {
+        Random random = new Random();
+        Integer randomNum = random.nextInt(200 - 0 + 1) + 0;
+        //synchronized (map) {
+            while (map.containsKey(randomNum)) {
+                randomNum = random.nextInt(200 - 0 + 1) + 0;
+            }
+            map.putIfAbsent(randomNum, integer);
+        //}
+    }
+}
