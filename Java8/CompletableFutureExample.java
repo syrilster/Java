2af06e7be7847b5da8b05bb3b0b3230903f44f79
Syril <syrilster@gmail.com>
2018-01-28 16:22:17 +0530

    Completable Future to call I/O in parallel

---
+++ Java8/CompletableFutureExample.java
@@ -0,0 +1,71 @@
+package Java8;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created by Syril on 28-01-2018.
+ */
+public class CompletableFutureExample {
+    public static void main(String[] args) {
+        long start = System.nanoTime();
+        CompletableFuture<String> callOne = CompletableFuture.supplyAsync(CompletableFutureExample::connectApiOne);
+        CompletableFuture<String> callTwo = CompletableFuture.supplyAsync(CompletableFutureExample::connectApiTwo);
+        CompletableFuture<String> callThree = CompletableFuture.supplyAsync(CompletableFutureExample::connectApiThree);
+        CompletableFuture<String> result = CompletableFuture.allOf(callOne, callTwo, callThree).
+                thenApplyAsync(aVoid -> convert(callOne.join(), callTwo.join(), callThree.join()));
+        /*try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+        try {
+            System.out.println(result.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        long duration = (System.nanoTime() - start) / 1_000_000;
+        System.out.print("Processed time\n" + duration);
+        //Using sequential
+        start = System.nanoTime();
+        String s1 = connectApiOne();
+        String s2 = connectApiTwo();
+        String s3 = connectApiThree();
+        System.out.println("\n" +convert(s1, s2, s3));
+        duration = (System.nanoTime() - start) / 1_000_000;
+        System.out.print("Processed time using sequential\n" + duration);
+    }
+
+    public static String convert(String s1, String s2, String s3) {
+        return s1 + " " + s2 + " " + s3;
+    }
+
+    public static String connectApiOne() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "SuccessFromOne";
+    }
+
+    public static String connectApiTwo() {
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "SuccessFromTwo";
+    }
+
+    public static String connectApiThree() {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "SuccessFromThree";
+    }
+}
