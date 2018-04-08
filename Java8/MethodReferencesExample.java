4ef764f7c3dbb1508418f087a59c42d7cbdecbb4
Syril Sadasivan <syrilster@gmail.com>
2018-04-08 15:43:35 +0530

    Update MethodReferencesExample.java

--- Java8/MethodReferencesExample.java
+++ Java8/MethodReferencesExample.java
@@ -13,8 +13,22 @@ public class MethodReferencesExample {
 	public static void main(String[] args) {
 		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-		System.out.println(numbers.stream()
-							.reduce(0, (total, e) -> Integer.sum(total, e)));
+		//Parameter as an argument. Note that println is not a static method but a instance in System.out.
+		numbers.forEach(System.out::println);
+		
+		//Parameter as an argument to static method
+		numbers.stream()
+			//.map(e -> String.valueOf(e)) can be replaced as below
+			.map(String::valueOf)
+			.forEach(System.out::println);
+		
+		//Parameter as a target on which a function is being called
+		numbers.stream()
+			.map(e -> e.toString())
+			.forEach(System.out::println);
+		
+		
+		System.out.println(numbers.stream().reduce(0, (total, e) -> Integer.sum(total, e)));
 
 		//Using Method Reference
 		System.out.println(numbers.stream()
