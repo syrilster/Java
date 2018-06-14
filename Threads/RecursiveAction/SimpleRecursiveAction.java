1c031e2f69c170f0bcd500b546af79cb32114fba
ssadasivan <syrilster@gmail.com>
2018-06-14 15:10:41 +0530

    Alignment Changes.

--- Threads/RecursiveAction/SimpleRecursiveAction.java
+++ Threads/RecursiveAction/SimpleRecursiveAction.java
@@ -7,22 +7,22 @@ import java.util.concurrent.RecursiveAction;
  * @since 3/5/2017.
  */
 public class SimpleRecursiveAction extends RecursiveAction {
-	private int simulatedWork;
+    private int simulatedWork;
 
-	public SimpleRecursiveAction(int simulatedWork) {
-		this.simulatedWork = simulatedWork;
-	}
+    public SimpleRecursiveAction(int simulatedWork) {
+        this.simulatedWork = simulatedWork;
+    }
 
-	@Override
-	protected void compute() {
-		if (simulatedWork > 100) {
-			System.out.println("Parallel Execution started.. " + simulatedWork);
-			SimpleRecursiveAction simpleRecursiveAction1 = new SimpleRecursiveAction(simulatedWork / 2);
-			SimpleRecursiveAction simpleRecursiveAction2 = new SimpleRecursiveAction(simulatedWork / 2);
-			simpleRecursiveAction1.fork();
-			simpleRecursiveAction2.fork();
-		}else{
-			System.out.println("No Need of parallel, sequential is OK  " + simulatedWork);
-		}
-	}
+    @Override
+    protected void compute() {
+        if (simulatedWork > 100) {
+            System.out.println("Parallel Execution started.. " + simulatedWork);
+            SimpleRecursiveAction simpleRecursiveAction1 = new SimpleRecursiveAction(simulatedWork / 2);
+            SimpleRecursiveAction simpleRecursiveAction2 = new SimpleRecursiveAction(simulatedWork / 2);
+            simpleRecursiveAction1.fork();
+            simpleRecursiveAction2.fork();
+        } else {
+            System.out.println("No Need of parallel, sequential is OK  " + simulatedWork);
+        }
+    }
 }
