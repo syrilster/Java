62ec929bb74ecd0ec32b676302563b087b739758
Syril <syrilster@gmail.com>
2018-07-12 20:18:01 +0530

    Added java doc for the countdown latch demo.

--- Threads/CountDownLatch/CountDownLatchDemo.java
+++ Threads/CountDownLatch/CountDownLatchDemo.java
@@ -2,6 +2,12 @@ package Threads.CountDownLatch;
 
 import java.util.concurrent.CountDownLatch;
 
+/**
+ * Read 3 files, parse the read lines and only after reading and parsing all the three files the application
+ * should move ahead to do some processing with the parsed objects.
+ * So here we have three separate threads reading three separate files and the main thread awaits
+ * until all the three threads finish and call countdown().
+ */
 public class CountDownLatchDemo {
     public static void main(String[] args) {
         CountDownLatch countDownLatch = new CountDownLatch(3);
