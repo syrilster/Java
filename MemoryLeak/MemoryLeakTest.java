38c56969c6fd8a60c9cbc83ff875d2e2d1d81904
Syril <syrilster@gmail.com>
2018-08-10 10:29:07 +1000

    Java Memory leak example updated.

--- MemoryLeak/MemoryLeakTest.java
+++ MemoryLeak/MemoryLeakTest.java
@@ -9,8 +9,7 @@ import java.util.Map;
 public class MemoryLeakTest {
     //Run this program with a low Java heap setting to get the exception quickly. i.e -Xmx64M
     @Test(expected = OutOfMemoryError.class)
-    public void givenMap_whenNoEqualsNoHashCodeMethods_thenOutOfMemory()
-            throws IOException, URISyntaxException {
+    public void givenMap_whenNoEqualsNoHashCodeMethods_thenOutOfMemory() {
         Map< Object, Object> map = System.getProperties();
         while (true) {
             map.put(new Key("key"), "value");
