1afbc568cbaa1a0da828b1a21f11ed33a611f850
Syril <syrilster@gmail.com>
2018-10-14 18:58:19 +1100

    Slight refactoring.

--- Arrays/MaxSubArrayContiguousAndNonContiguousSum.java
+++ Arrays/MaxSubArrayContiguousAndNonContiguousSum.java
@@ -4,7 +4,7 @@ package Arrays;
  * @author ssadasivan
  * @since 4/5/2017.
  */
-public class MaxSubArraySum {
+public class MaxSubArrayContiguousAndNonContiguousSum {
     public static void main(String[] args) {
         int[] array = {2, -1, 2, 3, 4, -5};
         System.out.println("Max sum contiguous : " + findMaxSumContiguous(array));
@@ -12,35 +12,33 @@ public class MaxSubArraySum {
     }
 
     private static int findMaxSumContiguous(int[] array) {
-        int sum = 0, ans = 0;
+        int sum = 0, finalSum = 0;
         for (int i = 0; i < array.length; i++) {
             if (sum + array[i] > 0) {
                 sum += array[i];
             } else {
                 sum = 0;
             }
-            ans = Math.max(ans, sum);
+            finalSum = Math.max(finalSum, sum);
         }
-        return ans;
+        return finalSum;
     }
 
     private static int findMaximumSum(int[] array) {
-        int result = 0;
-        int nonContSum = array[0];
+        int nonContiguousSum = array[0];
         for (int i = 1; i < array.length; i++) {
             //If sum is already negative and array[i] is a positive number then replace sum with array[i]
-            if (nonContSum < 0 && array[i] >= 0) {
-                nonContSum = array[i];
+            if (nonContiguousSum < 0 && array[i] >= 0) {
+                nonContiguousSum = array[i];
             }
             // If sum + current element is greater case
-            else if (nonContSum + array[i] > nonContSum) {
-                nonContSum += array[i];
+            else if (nonContiguousSum + array[i] > nonContiguousSum) {
+                nonContiguousSum += array[i];
             } //sum is already negative and array[i] is also a negative number higher than sum
-            else if (nonContSum < array[i] && nonContSum + array[i] < nonContSum) {
-                nonContSum = array[i];
+            else if (nonContiguousSum < array[i] && nonContiguousSum + array[i] < nonContiguousSum) {
+                nonContiguousSum = array[i];
             }
         }
-        result = nonContSum;
-        return result;
+        return nonContiguousSum;
     }
 }
