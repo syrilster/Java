e672b31d578b3f81bef136a701268b503d6f616e
Syril <syrilster@gmail.com>
2019-05-20 20:53:52 +1000

    minor update in the input.

--- Arrays/FindElementCountSortedArray.java
+++ Arrays/FindElementCountSortedArray.java
@@ -2,14 +2,14 @@ package Arrays;
 
 public class FindElementCountSortedArray {
     public static void main(String[] args) {
-        int[] arrayOne = {1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 9, 10};  // x = 9, ans = 4
+        int[] arrayOne = {1, 1, 2, 4, 5, 6, 7, 8, 9, 9, 9, 9, 10};  // x = 9, ans = 4
         int[] arrayTwo = {1, 1, 2, 2, 2, 2, 3};  // x = 3, ans = 1
 
         System.out.println("Given Element occurs " + findElementCount(arrayOne, 9) + " time(s)");
-        System.out.println("Given Element occurs " + findElementCount(arrayOne, 3) + " time(s)");
+        System.out.println("Given Element occurs " + findElementCount(arrayTwo, 3) + " time(s)");
     }
 
-    public static int findElementCount(int[] inputArray, int elementToSearch) {
+    private static int findElementCount(int[] inputArray, int elementToSearch) {
         int firstIndex = findElementBinarySearch(inputArray, elementToSearch, true);
         if (firstIndex == -1)
             return 0;
